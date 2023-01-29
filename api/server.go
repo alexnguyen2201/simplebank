846c7d86bc60a2d11f59bf6774ce9bb1beda0fb7
nguyenvanson2201 <Son.NV201910429@husteduvn.onmicrosoft.com>
2023-01-29 11:10:46 +0700

    update authentication

--- api/server.go
+++ api/server.go
@@ -1,35 +1,61 @@
 package api
 
 import (
+	"fmt"
+
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
 	"github.com/go-playground/validator/v10"
 	db "github.com/nguyenvanson2201/simplebank/db/sqlc"
+	"github.com/nguyenvanson2201/simplebank/token"
+	"github.com/nguyenvanson2201/simplebank/util"
 )
 
 // Server servers HTTP requests for our banking service
 type Server struct {
-	store  db.Store
-	router *gin.Engine
+	config     util.Config
+	store      db.Store
+	tokenMaker token.Maker
+	router     *gin.Engine
 }
 
-func NewServer(store db.Store) *Server {
-	server := &Server{store: store}
-	router := gin.Default()
+func NewServer(config util.Config, store db.Store) (*Server, error) {
+	tokenMaker, err := token.NewPasetoMaker(config.TokenSymmetricKey)
+	if err != nil {
+		return nil, fmt.Errorf("cannot create token maker: %w", err)
+	}
+
+	server := &Server{
+		config:     config,
+		store:      store,
+		tokenMaker: tokenMaker,
+	}
 
 	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
 		v.RegisterValidation("currency", validCurrency)
 	}
+	server.setupRouter()
+	return server, nil
+}
 
-	router.POST("/accounts", server.createAccount)
-	router.GET("/accounts/:id", server.getAccount)
-	router.GET("/accounts", server.listAccount)
+func (server *Server) setupRouter() {
+	router := gin.Default()
+
+	// for admin user
+	router.POST("/users", server.createUser)
 	router.PUT("/accounts/:id", server.updateAccount)
 
-	router.POST("/transfers", server.createTransfer)
+	//for normal user
+	router.POST("/users/login", server.loginUser)
+	authRoutes := router.Group("/").Use(authMiddleware(server.tokenMaker))
+
+	authRoutes.POST("/accounts", server.createAccount)
+	authRoutes.GET("/accounts/:id", server.getAccount)
+	authRoutes.GET("/accounts", server.listAccount)
+
+	authRoutes.POST("/transfers", server.createTransfer)
 
 	server.router = router
-	return server
 }
 
 // Start run the HTTP server on a specific address.
